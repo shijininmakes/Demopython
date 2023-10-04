eb6dad97df10be35973afcaa6715cae533387710
shijin shaji <shijinshaji75911@gmail.comgit config --global user.name SHIJIN>
2023-10-04 12:35:51 +0530

    added

--- websitescraper_project/websitescraper_project/settings.py
+++ websitescraper_project/websitescraper_project/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
